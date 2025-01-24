84340ce4555993eac618d2eb9a2c8f7bffe084db
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-25 04:39:59 +0530

    created clean iput function

---
+++ main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+)
+
+func cleanInput(text string) []string {
+	var list []string
+	list = strings.Split(text," ")
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+}
