b1e0e4b5e5a4091e0d9dc8b3d4601d35674657a9
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-25 11:45:31 +0530

    main test yes

--- main_test.go
+++ main_test.go
@@ -1,32 +1,32 @@
-// package main
+package main
 
-// import (
+import (
 
-// 	"testing"
-// )
-// func TestCleanInput(t *testing.T){
-// 	cases := []struct{
-// 		input string
-// 		expected string
-// 	}{
-// 		{
-// 			input: " hello world  ",
-// 		expected: "hello",
-// 		},
-// 	}
-// 	for _,c := range cases {
-// 		actual := cleanInput(c.input)
-// 		expected := c.expected
-// 		if actual!=expected {
-// 			t.Errorf("got %s want %s",actual,expected)
-// 		}
+	"testing"
+)
+func TestCleanInput(t *testing.T){
+	cases := []struct{
+		input string
+		expected string
+	}{
+		{
+			input: " hello world  ",
+		expected: "hello",
+		},
+	}
+	for _,c := range cases {
+		actual := cleanInput(c.input)
+		expected := c.expected
+		if actual!=expected {
+			t.Errorf("got %s want %s",actual,expected)
+		}
 
-// 		// for i := range actual {
-// 		// 	word:= actual[i]
-// 		// 	expectedWord := c.expected[i]
-// 		// 	if word != expectedWord {
-// 		// 		t.Errorf("got %s want %s",word,expectedWord)
-// 		// 	}
-// 		// }
-// 	}
-// }
+		// for i := range actual {
+		// 	word:= actual[i]
+		// 	expectedWord := c.expected[i]
+		// 	if word != expectedWord {
+		// 		t.Errorf("got %s want %s",word,expectedWord)
+		// 	}
+		// }
+	}
+}
