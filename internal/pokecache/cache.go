d9b1f967bd6e809470fb5ad505e264d80b32493a
Half-blood-prince-2710 <guptadmanish2710@gmail.com>
2025-01-25 11:46:10 +0530

    cache file yes

--- internal/pokecache/cache.go
+++ internal/pokecache/cache.go
@@ -1,58 +1,58 @@
-// package pokecache
+package pokecache
 
-// import (
-// 	"sync"
-// 	"time"
-// )
+import (
+	"sync"
+	"time"
+)
 
-// type cacheEntry struct {
-// 	createdAt time.Time
-// 	val []byte
-// }
+type cacheEntry struct {
+	createdAt time.Time
+	val []byte
+}
 
-// type Cache struct {
-// 	mp map[string]cacheEntry
-// 	mu sync.Mutex
-// }
+type Cache struct {
+	mp map[string]cacheEntry
+	mu sync.Mutex
+}
 
-// func NewCache(interval time.Duration) *Cache{
-// 	cache := &Cache{
-// 		mp: make(map[string]cacheEntry),
-// 	}
-// 	go cache.reapLoop(interval)
-// 	return cache
-// }
+func NewCache(interval time.Duration) *Cache{
+	cache := &Cache{
+		mp: make(map[string]cacheEntry),
+	}
+	go cache.reapLoop(interval)
+	return cache
+}
 
-// func (c *Cache) Add(key string, val []byte ){
-// 	c.mu.Lock()
-// 	defer c.mu.Unlock()
-// 	c.mp[key] = cacheEntry{val:val,createdAt: time.Now()}
-// } 
+func (c *Cache) Add(key string, val []byte ){
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	c.mp[key] = cacheEntry{val:val,createdAt: time.Now()}
+} 
 
-// func (c *Cache) Get(key string) ([]byte, bool) {
-// 	c.mu.Lock()
-// 	defer c.mu.Unlock()
-// 	if _,exists:=c.mp[key]; exists {
-// 		return c.mp[key].val, true
-// 	}else {
-// 		return nil, false
-// 	}
+func (c *Cache) Get(key string) ([]byte, bool) {
+	c.mu.Lock()
+	defer c.mu.Unlock()
+	if _,exists:=c.mp[key]; exists {
+		return c.mp[key].val, true
+	}else {
+		return nil, false
+	}
 
-// }
+}
 
-// func (c *Cache) reapLoop(interval time.Duration) {
+func (c *Cache) reapLoop(interval time.Duration) {
 
-// 	 for {
-// 			time.Sleep(interval)
-// 			c.mu.Lock()
-// 			if len(c.mp)>0 {
-// 				for key, _ := range c.mp {
-// 			if time.Since(c.mp[key].createdAt)> interval {
-// 				delete(c.mp,key)
-// 			}
-// 		}
-// 			}
+	 for {
+			time.Sleep(interval)
+			c.mu.Lock()
+			if len(c.mp)>0 {
+				for key, _ := range c.mp {
+			if time.Since(c.mp[key].createdAt)> interval {
+				delete(c.mp,key)
+			}
+		}
+			}
 		
-// 		c.mu.Unlock()
-// 	}
-// }
+		c.mu.Unlock()
+	}
+}
